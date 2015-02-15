ca52e5601d7b1c889e6199056d24f6800b8fe8ff
Craig <craig.saunders@me.com>
2015-02-15 22:08:19 +0000

    Setting up static file serving

--- dash/dash/urls.py
+++ dash/dash/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from dash.views import IndexView
 
 urlpatterns = patterns('',
     # Examples:
+    url('^.*$', IndexView.as_view(), name='index'),
     # url(r'^$', 'dash.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
