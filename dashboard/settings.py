58b5629fc2fcc51d036ca7cbd6ecd1598d22c814
craig <mrman2289@gmail.com>
2015-02-20 22:55:37 +0000

    API setup for displaying train times

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dashboard'
+    'dashboard',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -48,6 +49,14 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 ROOT_URLCONF = 'dashboard.urls'
 
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,14 +1,31 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers, serializers, viewsets
 from dashboard.models import TrainDeparture
 from dashboard.views import IndexView
 
 admin.site.register(TrainDeparture)
 
+
+class TrainDepartureSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TrainDeparture
+        fields = ('created_at', 'eta', 'destination', 'station')
+
+
+
+class TrainDepartureViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = TrainDeparture.objects.all()
+    serializer_class = TrainDepartureSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'departures', TrainDepartureViewSet)
+
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^admin/?', include(admin.site.urls)),
-    url('^.*$', IndexView.as_view(), name='index'),
-    # url(r'^$', 'dash.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       url('^/?$', IndexView.as_view(), name='index'),
+                       url(r'^api/', include(router.urls, namespace='api')),
+                       url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+                       url(r'^admin/?', include(admin.site.urls)),
 )
