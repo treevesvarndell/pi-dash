80badffa71b220c9bd66489e295ddd5d8f987812
craig <mrman2289@gmail.com>
2015-02-23 23:20:50 +0000

    Departures formatted properly

--- static/js/departures.ctrl.js
+++ static/js/departures.ctrl.js
@@ -9,17 +9,24 @@
     function DeparturesController(departuresService, $timeout, usSpinnerService) {
 
         var vm = this;
+        vm.people = ['mrman2289', 'Costelad', 'treevesvarndell'];
+
 
         (function getDepartures() {
             usSpinnerService.spin('departuresSpinner');
             return departuresService.getDepartures().then(function(data) {
                 console.log(data);
-                vm.departures = data;
+                vm.departuresEast = _.filter(data, function(train) { return train.direction === 'east' });
+                vm.departuresWest = _.filter(data, function(train) { return train.direction === 'west' });
                 $timeout(getDepartures, 15000);
                 usSpinnerService.stop('departuresSpinner');
                 return vm.departures;
             });
         })();
+
+        vm.getFacebookProfileImageUrl = function(username) {
+            return '//graph.facebook.com/' + username + '/picture?width=200';
+        }
 
     }
 
