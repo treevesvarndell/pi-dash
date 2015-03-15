497ef1ce9856e95211502143a745e58838a4e2d3
craig <mrman2289@gmail.com>
2015-03-15 14:03:16 +0000

    Formatting for a slightly smaller screen

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,14 @@
 (function() {
 
-    angular.module('app.dashboard', ['angularMoment', 'angularSpinner']);
+    angular.module('app.dashboard', ['angularMoment', 'angularSpinner'])
+
+    angular.module('app.dashboard').constant('angularMomentConfig', {
+        timezone: 'Europe/London',
+        withoutSuffix: true
+    });
+
+    angular.module('app.dashboard').constant('amTimeAgoConfig', {
+        withoutSuffix: true
+    });
 
 })();
