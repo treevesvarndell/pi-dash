908ffa16f75ee5bd11e96c9297f96f44e53cddcc
treevesvarndell <treevesvarn@gmail.com>
2015-05-17 22:39:00 +0100

    Set timezone as Europe/London, not UTC

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -84,11 +84,11 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
+USE_TZ = True
 
 USE_I18N = True
 USE_L10N = True
-USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
