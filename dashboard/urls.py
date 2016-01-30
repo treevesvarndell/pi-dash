e2c32883ec1cc63d792cee524970c9dcf3a8c54b
craig <mrman2289@gmail.com>
2016-01-30 21:58:24 +0000

    Dirty API to toggle the screen on/off

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from views import IndexView
+from views import IndexView, ScreenView
 
 
 urlpatterns = patterns('',
                        # Examples:
+                       url(r'^api/screen/?', ScreenView.as_view(), name='screen'),
                        url('^/?$', IndexView.as_view(), name='index'),
                        url(r'^admin/?', include(admin.site.urls)),
 )
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,30 @@
+import os
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic.base import TemplateView
 from django.utils.decorators import method_decorator
+from rest_framework import views
+from rest_framework.response import Response
+from subprocess import call
+
+
+class ScreenView(views.APIView):
+    permission_classes = []
+
+    def get(self, request):
+        return Response({'testing': True})
+
+    def post(self, request):
+
+        file_path = "/home/pi/dashboard/scripts/monitorToggle.sh"
+
+        if not os.path.isfile(file_path):
+            response = Response({'success': False, 'reason': 'File not found'})
+        else:
+            shell_action = 'on' if request.DATA.get('action', None) == 'on' else 'off'
+            system_call = call([file_path, shell_action])
+            response = Response({'success': system_call == 0})
+
+        return response
 
 
 class IndexView(TemplateView):
