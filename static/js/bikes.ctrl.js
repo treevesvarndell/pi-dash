41526b401175abd25a23b29cd31098b872be0180
Trevor Mac <treevesvarn@gmail.com>
2016-01-31 17:07:29 +0000

    Adding new Bikes display

---
+++ static/js/bikes.ctrl.js
@@ -0,0 +1,35 @@
+(function () {
+
+    angular
+        .module('app.dashboard')
+        .controller('BikesCtrl', BikesCtrl);
+
+    BikesCtrl.$inject = ['$http'];
+
+    function BikesCtrl($http) {
+        var vm = this;
+
+        function getBikes() {
+            $http.get("https://api-prod6.tfl.gov.uk/Place/BikePoints_547")
+                .success(function (data) {
+                    vm.numberOfBikes = data.additionalProperties.filter(availableBikes)[0].value;
+                    vm.numberOfSlots = data.additionalProperties.filter(spareDocks)[0].value;
+                })
+
+        }
+
+        function availableBikes(obj) {
+            if (obj.key === "NbBikes") {
+                return true;
+            }
+        }
+
+        function spareDocks(obj) {
+            if (obj.key === "NbEmptyDocks") {
+                return true;
+            }
+        }
+
+        getBikes()
+    }
+})();
